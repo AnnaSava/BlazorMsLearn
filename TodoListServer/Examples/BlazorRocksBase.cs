e7f649a911acb55f33bf52a1fd6641aa91fb7d46
Anna Savina <anna.s.savina@gmail.com>
2022-10-22 17:54:16 +0300

    Components Overview

---
+++ TodoListServer/Examples/BlazorRocksBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TodoListServer
+{
+    // https://learn.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-6.0
+    // Section Specify a base class
+    public class BlazorRocksBase : ComponentBase
+    {
+        public string BlazorRocksText { get; set; } =
+            "Blazor rocks the browser!";
+    }
+}
