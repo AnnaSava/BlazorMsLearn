6f771571a64bd3fa3e936994a9a825d60fee9d24
Anna Savina <anna.s.savina@gmail.com>
2022-10-28 21:57:56 +0300

    Persist prerendered state

--- Integrate/BlazorHosted/BlazorHosted/Server/Program.cs
+++ Integrate/BlazorHosted/BlazorHosted/Server/Program.cs
@@ -1,3 +1,4 @@
+using BlazorHosted.Shared;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
 
 var app = builder.Build();
 
