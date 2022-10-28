766e712b601ca28a15bd3f528d87c501732f119a
Anna Savina <anna.s.savina@gmail.com>
2022-10-28 21:44:29 +0300

    Counters

--- Integrate/BlazorHosted/BlazorHosted/Client/Program.cs
+++ Integrate/BlazorHosted/BlazorHosted/Client/Program.cs
@@ -1,8 +1,11 @@
 using BlazorHosted.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorHosted.Client.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<Counter>("#counter-component");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
