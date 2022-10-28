b034b3157ee93b7af1bdcb977482bf8269a4078a
Anna Savina <anna.s.savina@gmail.com>
2022-10-28 18:09:02 +0300

    Interactive and noninteractive rendering

---
+++ Integrate/BlazorServerRazorPages/BlazorServerRazorPages/Pages/NonInteractive.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorServerRazorPages.Pages
+{
+    public class NonInteractiveModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
