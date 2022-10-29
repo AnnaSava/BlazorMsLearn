21ac0d2bbfc7df8ba7134d50143b3445e855c192
Anna Savina <anna.s.savina@gmail.com>
2022-10-29 17:24:02 +0300

    Statically set culture, Wasm

--- TodoListWasm/Program.cs
+++ TodoListWasm/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
 using TodoListWasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
 await builder.Build().RunAsync();
