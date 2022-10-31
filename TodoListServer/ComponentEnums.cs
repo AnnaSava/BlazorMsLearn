b4fb95d54f36f43b8e9bb2b5d41aaa5b16d91427
Anna Savina <anna.s.savina@gmail.com>
2022-10-31 21:48:26 +0300

    Forms and input components

---
+++ TodoListServer/ComponentEnums.cs
@@ -0,0 +1,6 @@
+﻿public class ComponentEnums
+{
+    public enum Manufacturer { SpaceX, NASA, ULA, VirginGalactic, Unknown }
+    public enum Color { ImperialRed, SpacecruiserGreen, StarshipBlue, VoyagerOrange }
+    public enum Engine { Ion, Plasma, Fusion, Warp }
+}
---
+++ TodoListServer/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ExampleModel
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Name is too long.")]
+    public string? Name { get; set; }
+
+    [StringLength(10, ErrorMessage = "Description is too long.")]
+    public string? Description { get; set; }
+}
