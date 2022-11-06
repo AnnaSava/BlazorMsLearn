94c8128260240d8f0ab757af7bdfec91f9adcdae
Anna Savina <anna.s.savina@gmail.com>
2022-11-06 15:03:56 +0300

    Call .Net from JS

---
+++ TodoListServer/MessageUpdateInvokeHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+public class MessageUpdateInvokeHelper
+{
+    private Action action;
+
+    public MessageUpdateInvokeHelper(Action action)
+    {
+        this.action = action;
+    }
+
+    [JSInvokable("TodoListServer")]
+    public void UpdateMessageCaller()
+    {
+        action.Invoke();
+    }
+}
---
+++ TodoListWasm/GenericType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+public class GenericType<TValue>
+{
+    public TValue? Value { get; set; }
+
+    [JSInvokable]
+    public void Update(TValue newValue)
+    {
+        Value = newValue;
+
+        Console.WriteLine($"Update: GenericType<{typeof(TValue)}>: {Value}");
+    }
+
+    [JSInvokable]
+    public async void UpdateAsync(TValue newValue)
+    {
+        await Task.Yield();
+        Value = newValue;
+
+        Console.WriteLine($"UpdateAsync: GenericType<{typeof(TValue)}>: {Value}");
+    }
+}
